1b3f83137c3468acef278c708b1783ac009a2ffc
perezgoa <aperezgo74@gmail.com>
2016-03-01 16:21:28 +0100

    NTM

---
+++ app/src/main/java/com/example/srava/menuusmb/Categories.java
@@ -0,0 +1,24 @@
+package com.example.srava.menuusmb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by perezgoa on 01/03/2016.
+ */
+public class Categories {
+    public List<Categorie> listeCategories;
+
+    public Categories() {
+        listeCategories=new ArrayList<Categorie>();
+    }
+
+    public Categories(Categorie categorie) {
+        listeCategories.add(categorie);
+    }
+
+    @Override
+    public String toString() {
+        return "A completer";
+    }
+}
